14da67da0b2d31031c05833694e2dc0de37022af
Oware-Ebenezer <nanaprestige71@gmail.com>
2026-02-25 12:58:18 +0000

    add tabs for proper navigation

---
+++ app/(tabs)/calendar.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function CalendarScreen() {
+  return (
+     <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-xl font-bold">Calendar Screen</Text>
+    </View>
+  );
+}
